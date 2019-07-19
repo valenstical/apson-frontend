88039fe4b6dcf18d5806f6bc3ed8f43e2f727089
Valentine Okosun <ejodamen.okosun@andela.com>
2019-07-19 22:12:01 +0100

    restart app

---
+++ projects/visitors/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './projects/visitors/home/home.component';
+import { AboutComponent } from './projects/visitors/about/about.component';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, AboutComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
